a182233a58556298776f5d48c656c132f2e4c2d6
KUYAKENN <kennethjhonaycardo@gmail.com>
2025-09-19 10:19:16 +0800

    psuh

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     uvicorn.run(
         'src.app_module:app',  # Correctly reference the FastAPI instance
         host="0.0.0.0",
-        port=5003,  # Keep your original port
+        port=5004,  # Keep your original port
         reload=True
     )
 
