4567f010a0b75da447252bcf3948706dbb46099f
KUYAKENN <kennethjhonaycardo@gmail.com>
2025-09-19 11:23:20 +0800

    push

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import uvicorn
 
 if __name__ == '__main__':
     uvicorn.run(
-        'michael_app:app',  # Reference the Michael-specific FastAPI instance
+        'kenneth_app:app',  # Reference the Kenneth FastAPI instance
         host="0.0.0.0",
-        port=5003,
+        port=5004,
         reload=True
     )
 
